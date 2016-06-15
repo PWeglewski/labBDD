762c47ce7bd8c6fd312a898aba2ba48022097adc
Piotr Weglewski <190160@edu.p.lodz.pl>
2016-06-15 11:05:31 +0200

    Implemented when step in arrival extimation test

--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/ArrivalEstimationSteps.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/ArrivalEstimationSteps.java
@@ -1,6 +1,5 @@
 package edu.iis.mto.bdd.trains.cucumber.steps;
 
-import cucumber.api.PendingException;
 import cucumber.api.Transform;
 import cucumber.api.java.pl.Gdy;
 import cucumber.api.java.pl.I;
@@ -20,24 +19,31 @@ public class ArrivalEstimationSteps {
 
     private String line;
     private String destination;
+    private String currentLocation;
+    private LocalTime departureTime;
+
+    private LocalTime actualArrivalTime;
 
     @Zakładając("że chcę się dostać z (.*) do (.*)$")
     public void givenLocationAndDestination(String currentLocation, String destination) throws Throwable {
         this.destination = destination;
+        this.currentLocation = currentLocation;
     }
 
     @I("^następny pociąg odjeżdża o (.*) na linii (.*)$")
     public void andNextTrainDepartureTimeAndLine(@Transform(JodaLocalTimeConverter.class) LocalTime departureTime, String line) throws Throwable {
         this.line = line;
+        this.departureTime = departureTime;
+        timetableService.scheduleArrivalTime(line, departureTime);
     }
 
     @Gdy("^zapytam o godzinę przyjazdu$")
     public void whenIaskAboutArrivalTime() throws Throwable {
+        actualArrivalTime = timetableService.getArrivalTime(line, destination);
     }
 
     @Wtedy("^powinienem uzyskać następujący szacowany czas przyjazdu: (.*)$")
     public void shouldBeInformedAboutEstimatedArrivalTime(@Transform(JodaLocalTimeConverter.class) LocalTime expectedArrivalTime) throws Throwable {
-        LocalTime actualArrivalTime = timetableService.getArrivalTime(line, destination);
         assertThat(actualArrivalTime, equalTo(expectedArrivalTime));
     }
 }
